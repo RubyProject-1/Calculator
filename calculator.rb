252e66e02cc161a09d4bd29b30baf166b2dc32a7
Krys <21113012+Krystade@users.noreply.github.com>
2021-10-12 16:01:31 +0200

    Added non functioning UI and BEGIN

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,28 @@
-# @author Rosalinda Chamale,
+# @author Rosalinda Chamale, Jack Schmid
+require 'ruby2d'
+
+# Set the window size
+set width: 500, height: 378
+# Set the background color
+set background: '#376278'
+# Set the window title
+set title: "Calculator"
+# Create the buttons
+buttons = []
+buttons[0] = Button.new(2, 2, 496, 100, "#505050", "Type")
+buttons[1] = Button.new(2, 72, 246, 100, "#FF9500", "+")
+buttons[2] = Button.new(248, 72, 250, 100, "#FF9500", "-")
+buttons[3] = Button.new(2, 174, 165, 100, "#FF9500", "x")
+buttons[4] = Button.new(167, 174, 165, 100, "#FF9500", "/")
+buttons[5] = Button.new(334, 174, 164, 100, "#FF9500", "^")
+buttons[6] = Button.new(2, 276, 496, 100, "#FF9500", "=")
+update do
+
+end
+show
+user_interface()
+
+BEGIN{
 def menu
     puts "\nChoose an option by inputting a number between 1 and 6"
     puts "1. Add the numbers"
@@ -11,7 +35,6 @@ def menu
     @choice = gets.to_i #choice is instance variable, can be used outside of function
     puts("\n")
 end
-
 def recalculate
     puts "\nDo you want to do another operation on the same numbers? (enter 7)"
     puts "Do you want to do another operation on different numbers? (enter 8)"
@@ -77,5 +100,33 @@ def user_interface
     end
 end
 
-user_interface()
-
+class Button
+    @@numOfButtons = 0
+    def initialize(x, y, width, height, color, label)
+        @x = x
+        @y = y 
+        @width = width
+        @height = height 
+        @color = color
+        @outlineWidth = 4
+        @outline = Rectangle.new(
+            x: @x - @outlineWidth/2, y: @y - @outlineWidth/2,
+            width: @width + @outlineWidth, height: @height + @outlineWidth,
+            color: 'black'
+        )
+        @rect = Rectangle.new(
+            x: @x, y: @y,
+            width: @width, height: @height,
+            color: @color
+        )
+        @label = Text.new(
+            label,
+            x: x + width/2 - 6, y: y + height/2 - 15,
+            size: 20,
+            color: 'black'
+            )
+            
+        @@numOfButtons = @@numOfButtons + 1
+    end
+end
+}
