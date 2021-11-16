4c39861879053b90b9618b90fd98a6a076c433d2
Ricky <ricky.y.fok@gmail.com>
2021-11-16 11:14:38 -0800

    Shopping Calculator Concept
    Loose implementation of shopping calculator concept

---
+++ shoppingItem.rb
@@ -0,0 +1,27 @@
+class ShoppingItem
+
+  def initialize(name, price, quantity)
+    @name = name
+    @price = price
+    @quantity = quantity
+    @tPrice = price * quantity
+  end
+
+  attr_accessor :name
+  attr_accessor :price
+  attr_accessor :quantity
+  #attr_accessor :tPrice
+
+
+  def updatePrice # updates price for changed quantity/price
+    @tPrice = price * quantity
+  end
+
+  def tPrice
+    updatePrice()
+    @tPrice
+  end
+    
+end
+
+
