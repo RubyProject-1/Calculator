cff662e4d5a63b2038ecfa67da2c189c654d8ca9
ricardoaxelbaeza <ricardobaeza2000@gmail.com>
2021-11-19 13:38:58 -0800

    revert price calculations

--- priceCalculation.rb
+++ priceCalculation.rb
@@ -1,36 +1,15 @@
-require_relative 'shoppingItem'
-listOfItems = []
-$discount = false
-$discountAmt = 0
-def calculate (state, arr, sale = 0, total = 0)
-    print "Shopping List\n-------------\n"
-    for i in arr  # sums totals
-      total = total + i.tPrice
-    end
-    
-    subtotal = priceCalculate(total,sale).round(2) #calculates subtotal
+def calculate (state, price, sale = 0)
+    subtotal = priceCalculate(price,sale).round(2) #calculates subtotal
     salesTax = taxCalulate(subtotal,state) #calculates sales tax amount
-    printItems(arr)  #prints names of items in array
-
-    if $discount == true  #prints if there is discount
-      print "---DISCOUNT!---\n"
-      print "You saved $", $discountAmt, "!\n"
-    end
-
-    print "-------------\nSubtotal: $", subtotal
+    print "subtotal: ", subtotal
     total = totalCalculate(subtotal,salesTax).round(2) #calulates total price of item
     puts
-    print "Total: $", total, "\n\n"
+    print "total: ", total
 end #end function
 
 def priceCalculate(price,sale)
-    $discountAmt = discountCalculate(price,sale)
-    price = price - $discountAmt
-    
-    if sale > 0
-      $discount = true
-    end
-
+    discountAmt = discountCalculate(price,sale)
+    price = price - discountAmt
     return price
 end
 
@@ -63,31 +42,14 @@ def taxCalulate(price,state)
     end #end if
 end
 
-def printItems(arr)
-  for i in arr
-    puts "$" + i.name.ljust(10) + i.tPrice.to_s
-  end
-end
-#main code
-item1 = ShoppingItem.new("Eggs", 4.20, 2)
-item2 = ShoppingItem.new("Chicken", 5.10, 4)
-item3 = ShoppingItem.new("Milk", 3.45, 2)
 
-listOfItems.append(item1)
-listOfItems.append(item2)
-listOfItems.append(item3)
 
-calculate("California", listOfItems)
-calculate("Alaska", listOfItems, 10)
 
-  
-    
-    
 
-#calculate("California", 100.00,10)
-#puts
-#puts
-#calculate("California", 100.00)
+calculate("California", 100.00,10)
+puts
+puts
+calculate("California", 100.00)
 
 
 # puts 20/100.to_f
@@ -104,4 +66,4 @@ calculate("Alaska", listOfItems, 10)
 #         price = price - discount
 #         subtotal = price
 #         total = price + salesTax
-#     end #end of if statement
+#     end #end of if statement 
